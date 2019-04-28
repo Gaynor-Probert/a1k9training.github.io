7e5a20c7334165418c13ddb250696bd464136b0f
Courtenay Probert <courtenay@probert.me.uk>
2019-04-28 12:50:08 +0100

    Cleaner

--- _build.bat
+++ _build.bat
@@ -4,6 +4,10 @@ set "wd=%cd%"
 git checkout master
 
 @call scms --destination %temp%/a1k9
+del %temp%\a1k9\_includes /f/s/q
+del %temp%\a1k9\_layouts /f/s/q
+del %temp%\a1k9\_pages /f/s/q
+del %temp%\a1k9\_* /f/s/q
 @call git checkout gh-pages
 
 echo %wd%
