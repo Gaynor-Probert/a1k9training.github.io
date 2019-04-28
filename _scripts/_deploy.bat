b24ef77e9808f58f37fd87d67dce9e7ae8315b25
Courtenay Probert <courtenay@probert.me.uk>
2019-04-28 13:49:22 +0100

    Update _deploy.bat

--- _scripts/_deploy.bat
+++ _scripts/_deploy.bat
@@ -11,13 +11,19 @@ echo Cleaning unwanted debris
 del %temp%\a1k9\_includes /f/s/q
 del %temp%\a1k9\_layouts /f/s/q
 del %temp%\a1k9\_pages /f/s/q
+
+rd /s /q %temp%\a1k9\_includes\
+rd /s /q %temp%\a1k9\_layouts\
+rd /s /q %temp%\a1k9\_pages\
+rd /s /q %temp%\a1k9\_scripts
+
 del %temp%\a1k9\_* /f/s/q
 
 echo Swithcing branch
 @call git checkout gh-pages
 
 echo Copying temp files to gp-pages
-move %temp%/a1k9/ %wd%/
+copy %temp%/a1k9/ %wd%/
 
 echo Done
 start %temp%/a1k9
