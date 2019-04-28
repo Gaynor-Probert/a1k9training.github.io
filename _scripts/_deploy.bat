8fe382752da927be6e66caa85bdcb6422f632fba
Courtenay Probert <courtenay@probert.me.uk>
2019-04-28 13:38:50 +0100

    Update _deploy.bat

--- _scripts/_deploy.bat
+++ _scripts/_deploy.bat
@@ -5,14 +5,21 @@ set "wd=%cd%"
 echo %wd%
 
 @call scms --destination %temp%/a1k9
+echo Built
+
+echo Cleaning unwanted debris
 del %temp%\a1k9\_includes /f/s/q
 del %temp%\a1k9\_layouts /f/s/q
 del %temp%\a1k9\_pages /f/s/q
 del %temp%\a1k9\_* /f/s/q
 
+echo Swithcing branch
 @call git checkout gh-pages
 
-move %temp%/a1k9 %wd%
+echo Copying temp files to gp-pages
+move %temp%/a1k9/ %wd%/
+
+echo Done
 start %temp%/a1k9
 start %wd%
 
