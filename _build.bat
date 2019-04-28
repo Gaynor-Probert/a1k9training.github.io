895b5f9c464b4ab504dfc6c5ed6fc094a4b2af99
Courtenay Probert <courtenay@probert.me.uk>
2019-04-28 12:53:07 +0100

    Update _build.bat

--- _build.bat
+++ _build.bat
@@ -8,6 +8,7 @@ del %temp%\a1k9\_includes /f/s/q
 del %temp%\a1k9\_layouts /f/s/q
 del %temp%\a1k9\_pages /f/s/q
 del %temp%\a1k9\_* /f/s/q
+copy %wd%\_build.bat  %temp%\a1k9\_build.bat
 @call git checkout gh-pages
 
 echo %wd%
