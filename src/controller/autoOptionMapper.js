8967c5c480cabfb856d25358cd5fcfb88809958f
Courtenay Probert <courtenay@probert.me.uk>
2020-11-06 23:02:23 +0000

    Update autoOptionMapper.js

--- src/controller/autoOptionMapper.js
+++ src/controller/autoOptionMapper.js
@@ -1,8 +1,12 @@
+const fs = require("fs");
+
 module.exports = (options) => {
   const view = options.view;
   const mappedModel = view.replace(".hbs", ".json");
+  const modelText = fs.readFileSync(`./src/models/${mappedModel}`, "utf8");
+  const model = JSON.parse(modelText);
   return {
-    title: options.model.title,
-    model: mappedModel,
+    title: model.title,
+    model: model,
   };
 };
