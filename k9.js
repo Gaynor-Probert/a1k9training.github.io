9b84b459491c448d30e937dab0b3bc5b610cadcd
Courtenay Probert <courtenay@probert.me.uk>
2020-11-06 18:04:30 +0000

    New site

---
+++ k9.js
@@ -0,0 +1,22 @@
+const Kiss = require("kiss-ssg");
+const kiss = new Kiss();
+
+kiss
+  .pages(
+    {
+      view: "course.hbs",
+      model: "courses",
+      nav: true,
+    },
+    ({ model }) => {
+      return {
+        slug: model.slug,
+        title: model.title,
+        model: model,
+      };
+    }
+  )
+  .page({
+    view: "index.hbs",
+    model: "index.json",
+  });
