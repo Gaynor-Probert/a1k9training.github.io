6041e53e99d2192561f8c06c08322bbe2245c651
Courtenay Probert <courtenay@probert.me.uk>
2020-11-06 22:39:22 +0000

    Added external controllers

--- gen.js
+++ gen.js
@@ -1,26 +1,39 @@
 const Kiss = require("./src/kiss-ssg.js");
 const kiss = new Kiss({ dev: true });
 
+const slugMapper = ({ model }) => {
+  return {
+    slug: model.slug,
+  };
+};
+const courseController = ({ model }) => {
+  return {
+    slug: model.slug,
+    title: model.title,
+    model: model,
+  };
+};
+
 kiss
-  .page({
-    view: "index.hbs",
-    model: "index.json",
-  })
-  .page({
-    view: "find-us.hbs",
-    model: "find-us.json",
-  })
+  .page(
+    {
+      view: "index.hbs",
+      model: "index.json",
+    },
+    slugMapper
+  )
+  .page(
+    {
+      view: "find-us.hbs",
+      model: "find-us.json",
+    },
+    slugMapper
+  )
   .pages(
     {
       view: "course.hbs",
       model: "courses",
       nav: true,
     },
-    ({ model }) => {
-      return {
-        slug: model.slug,
-        title: model.title,
-        model: model,
-      };
-    }
+    courseController
   );
