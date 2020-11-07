8e298df6d892c106d62bc55de9e04a2a147349e5
Courtenay Probert <courtenay@probert.me.uk>
2020-11-07 14:42:34 +0000

    Added controller support to kiss and simplifies the generator

--- gen.js
+++ gen.js
@@ -1,25 +1,18 @@
 const Kiss = require('./src/kiss-ssg.js')
 const kiss = new Kiss({ dev: true })
 
-const autoOptionMapper = require('./src/controller/autoOptionMapper.js')
 kiss
-  .page(
-    {
-      view: 'index.hbs',
-    },
-    autoOptionMapper
-  )
-  .page(
-    {
-      view: 'find-us.hbs',
-    },
-    autoOptionMapper
-  )
-  .pages(
-    {
-      view: 'course.hbs',
-      model: 'courses',
-    },
-    require('./src/controller/course.js')
-  )
-  .viewState()
+  .page({
+    model: 'index.json',
+    view: 'index.hbs',
+  })
+  .page({
+    model: 'find-us.json',
+    view: 'find-us.hbs',
+  })
+  .pages({
+    model: 'courses',
+    view: 'course.hbs',
+    controller: 'course.js',
+  })
+// .viewState()
