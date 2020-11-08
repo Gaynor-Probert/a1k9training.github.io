2901b223ce6399a465f566518c5087b0f33c5392
Courtenay Probert <courtenay@probert.me.uk>
2020-11-08 18:50:21 +0000

    behavioural-consultations

--- gen.js
+++ gen.js
@@ -3,22 +3,49 @@ const kiss = new Kiss({ dev: true })
 
 kiss
   .page({
-    model: 'index.json',
     view: 'index.hbs',
     controller: 'metaMapper.js',
+    title: 'Dog Behaviour and Training',
+    description: 'A1K9 Dog Behaviour and Training',
   })
   .page({
-    model: 'find-us.json',
+    view: 'behavioural-consultations/index.hbs',
+    model: {
+      image: '/images/consultations/consultations-v1.png',
+      'caption-class': 'pull-right',
+    },
+    title: 'Dog Behavioural Consultations - by Gaynor Probert',
+  })
+  .page({
+    view: 'behavioural-consultations/dog-on-dog-aggression.hbs',
+    model: {
+      image:
+        '/images/consultations/dog-on-dog-aggression/dog-on-dog-aggression-v1.png',
+      'caption-class': 'pull-right',
+    },
+    title: 'Behavioural Consultations for Dog on Dog Aggression',
+  })
+  .page({
+    view: 'behavioural-consultations/dog-on-person-aggression.hbs',
+    model: {
+      image:
+        '/images/consultations/dog-on-person-aggression/dog-on-person-aggression-v3.png',
+      'caption-class': 'pull-left',
+    },
+    title: 'Behavioural Consultations for Dog on Person Aggression',
+  })
+  .page({
     view: 'find-us.hbs',
+    model: {
+      image: '/images/about/horse-sit-v1.png',
+      'caption-class': 'pull-right push-down',
+    },
+    title: 'Find A1K9',
   })
   .pages({
-    model: 'courses',
     view: 'course.hbs',
-    controller: 'course.js',
+    model: 'courses',
   })
   .complete(() => {
     console.log('Success'.rainbow)
-  })
-  .then(() => {
-    console.log('Ready to publish'.brightCyan)
   })
